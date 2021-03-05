9e9c3d774046549b68bf2f4e7edca372cb3ad0a4
Akshay Sharma <akshaysharmaay@gmail.com>
2021-03-05 20:16:38 +0530

    Added AddTransaction & replace id with className

--- src/components/IncomeExpenses.js
+++ src/components/IncomeExpenses.js
@@ -1,20 +1,16 @@
 const IncomeExpenses = () => {
-  return (
-    <div class="inc-exp-container">
-      <div>
-        <h4>Income</h4>
-        <p id="money-plus" class="money plus">
-          +₹0.00
-        </p>
-      </div>
-      <div>
-        <h4>Expense</h4>
-        <p id="money-minus" class="money minus">
-          -₹0.00
-        </p>
-      </div>
-    </div>
-  );
+	return (
+		<div className="inc-exp-container">
+			<div>
+				<h4>Income</h4>
+				<p className="money plus">+₹0.00</p>
+			</div>
+			<div>
+				<h4>Expense</h4>
+				<p className="money minus">-₹0.00</p>
+			</div>
+		</div>
+	);
 };
 
 export default IncomeExpenses;
--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -1,15 +1,15 @@
 const TransactionList = () => {
-  return (
-    <>
-      <h3>History</h3>
-      <ul id="list" class="list">
-        <li class="minus">
-          Cash <span>-₹400</span>
-          <button class="delete-btn">x</button>
-        </li>
-      </ul>
-    </>
-  );
+	return (
+		<>
+			<h3>History</h3>
+			<ul className="list">
+				<li className="minus">
+					Cash <span>-₹400</span>
+					<button className="delete-btn">x</button>
+				</li>
+			</ul>
+		</>
+	);
 };
 
 export default TransactionList;
