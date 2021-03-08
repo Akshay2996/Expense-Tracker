ccb831752503cbf4fcc7339a3785d232e3bef757
Akshay Sharma <akshaysharmaay@gmail.com>
2021-03-08 20:14:39 +0530

    Added Reducer Functionality

--- src/App.js
+++ src/App.js
@@ -5,18 +5,20 @@ import IncomeExpenses from "./components/IncomeExpenses";
 import TransactionList from "./components/TransactionList";
 import AddTransaction from "./components/AddTransaction";
 
+import { GlobalProvider } from "./context/GlobalState";
+
 function App() {
-  return (
-    <div>
-      <Header />
-      <div className="container">
-        <Balance />
-        <IncomeExpenses />
-        <TransactionList />
-        <AddTransaction />
-      </div>
-    </div>
-  );
+	return (
+		<GlobalProvider>
+			<Header />
+			<div className="container">
+				<Balance />
+				<IncomeExpenses />
+				<TransactionList />
+				<AddTransaction />
+			</div>
+		</GlobalProvider>
+	);
 }
 
 export default App;
