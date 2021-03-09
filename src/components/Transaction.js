ee4a742de5e22d1ca9fdcf9273cad20b4488b1d1
Akshay Sharma <akshaysharmaay@gmail.com>
2021-03-09 20:21:04 +0530

    Added Transaction component while passing props

---
+++ src/components/Transaction.js
@@ -0,0 +1,10 @@
+const Transaction = ({ transaction }) => {
+	return (
+		<li className="minus">
+			{transaction.text} <span>-₹400</span>
+			<button className="delete-btn">x</button>
+		</li>
+	);
+};
+
+export default Transaction;
