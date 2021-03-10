3f888f60af9869943f2576155c5a364db59b233a
Akshay Sharma <akshaysharmaay@gmail.com>
2021-03-10 20:20:50 +0530

    Added automatic updation of Balance

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -1,8 +1,21 @@
+import { useContext } from "react";
+import { GlobalContext } from "../context/GlobalState";
+
 const Balance = () => {
+	const { transactions } = useContext(GlobalContext);
+
+	const amounts = transactions.map((transaction) => {
+		return transaction.amount;
+	});
+
+	const total = amounts
+		.reduce((acc, current) => (acc += current), 0)
+		.toFixed(2);
+
 	return (
 		<>
 			<h4 style={{ textAlign: "center" }}>Your Balance</h4>
-			<h1 style={{ textAlign: "center" }}>₹0.00</h1>
+			<h1 style={{ textAlign: "center" }}>₹{total}</h1>
 		</>
 	);
 };
--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -4,9 +4,9 @@ import AppReducer from "./AppReducer";
 // Initial State
 const initialState = {
 	transactions: [
-		{ id: 1, text: "Flower", amount: -200 },
+		{ id: 1, text: "Flower", amount: -2000 },
 		{ id: 2, text: "Salary", amount: 30000 },
-		{ id: 3, text: "Book", amount: -1000 },
+		{ id: 3, text: "Book", amount: -10000 },
 		{ id: 4, text: "Camera", amount: 1500 },
 	],
 };
