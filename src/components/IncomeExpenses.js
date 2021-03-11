bafda10e656b47390c5a4303177c7dde67b7224c
Akshay Sharma <akshaysharmaay@gmail.com>
2021-03-11 12:59:39 +0530

    Added the income & expense tab

--- src/components/IncomeExpenses.js
+++ src/components/IncomeExpenses.js
@@ -1,13 +1,31 @@
+import { useContext } from "react";
+import { GlobalContext } from "../context/GlobalState";
+
 const IncomeExpenses = () => {
+	const { transactions } = useContext(GlobalContext);
+
+	const amounts = transactions.map((transaction) => transaction.amount);
+
+	const income = amounts
+		.filter((amount) => amount > 0)
+		.reduce((acc, amount) => (acc += amount), 0)
+		.toFixed(2);
+
+	const expense = (
+		amounts
+			.filter((amount) => amount < 0)
+			.reduce((acc, amount) => (acc += amount), 0) * -1
+	).toFixed(2);
+
 	return (
 		<div className="inc-exp-container">
 			<div>
 				<h4>Income</h4>
-				<p className="money plus">+₹0.00</p>
+				<p className="money plus">{income}</p>
 			</div>
 			<div>
 				<h4>Expense</h4>
-				<p className="money minus">-₹0.00</p>
+				<p className="money minus">{expense}</p>
 			</div>
 		</div>
 	);
